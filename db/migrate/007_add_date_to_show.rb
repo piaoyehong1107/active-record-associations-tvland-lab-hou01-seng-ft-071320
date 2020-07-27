c34bda526b8e31d96bf11c448178bd432a89b90b
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-27 17:57:18 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_date_to_show.rb
+++ db/migrate/007_add_date_to_show.rb
@@ -1,4 +1,4 @@
-class AddDateToShow < ActiveRecor::Migration[5.1]
+class AddDateToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
