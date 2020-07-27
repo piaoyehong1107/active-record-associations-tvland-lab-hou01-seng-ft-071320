fefb1783cd3b4b79daa9febeaa3f62d9e1e1c4de
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-27 17:11:46 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    list=[]
+    list.push(Character.all.find_by(actor:self).actor.full_name)
+    list
+  end
+  
+  def build_network(m)
+    newnetwork=Network.create(m)
+    self.network=newnetwork
+  end
+    
 end
