90ddfaff826d0b3406556721523ccf3c82409bc8
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-27 17:56:57 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_date_to_show.rb
+++ db/migrate/007_add_date_to_show.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < ActiveRecor::Migration[5.1]
+class AddDateToShow < ActiveRecor::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
